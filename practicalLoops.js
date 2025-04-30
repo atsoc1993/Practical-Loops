a61aaa0f2bc64cc32a5cea32247658b489ee37ff
atsoc1993 <atsoc1993@gmail.com>
2025-04-30 15:42:31 -0400

    Uncommented out code

--- practicalLoops.js
+++ practicalLoops.js
@@ -1,68 +1,77 @@
-// /*
-// Part 1: Fizz Buzz
-// To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
-// Accomplish the following:
+/*
+Part 1: Fizz Buzz
+To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
+Accomplish the following:
 
-// Loop through all numbers from 1 to 100.
-// If a number is divisible by 3, log “Fizz.”
-// If a number is divisible by 5, log “Buzz.”
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-// If a number is not divisible by either 3 or 5, log the number.
-// Remember to commit your solution once it is working.
-// */
+Loop through all numbers from 1 to 100.
+If a number is divisible by 3, log “Fizz.”
+If a number is divisible by 5, log “Buzz.”
+If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+If a number is not divisible by either 3 or 5, log the number.
+Remember to commit your solution once it is working.
+*/
 
-// for (let i=1; i <= 100; i++) {
-//     if (i % 3 == 0 && i % 5 == 0) { 
-//         console.log("Fizz Buzz.");
+for (let i=1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) { 
+        console.log("Fizz Buzz.");
 
-//     } else if (i % 5 == 0) {
-//         console.log("Buzz.");
+    } else if (i % 5 == 0) {
+        console.log("Buzz.");
         
-//     } else if (i % 3 == 0) {
-//         console.log("Fizz.");
+    } else if (i % 3 == 0) {
+        console.log("Fizz.");
 
-//     } else {
-//         console.log(i);
-//     }
-// }
+    } else {
+        console.log(i);
+    }
+}
 
-// /*
-// Part 2: Prime Time
-// Now we will move onto something slightly more complex.
-// Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example,
-//  the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. 
-//  As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+/*
+Part 2: Prime Time
+Now we will move onto something slightly more complex.
+Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example,
+ the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. 
+ As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
 
-//  Write a script that accomplishes the following:
-// Declare an arbitrary number, n.
-// Create a loop that searches for the next prime number, starting at n and incrementing from there.
-// As soon as you find the prime number, log that number and exit the loop.
+ Write a script that accomplishes the following:
+Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop.
 
-// Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11.
-//  Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
-// Be careful! If you set n to a number too large, your loop could take a long time to process.
-// */
+Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11.
+ Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+Be careful! If you set n to a number too large, your loop could take a long time to process.
+*/
 
-// let n = 18;
+let n = 18;
 
-// //Part 1
-// for (let i=n; i >= n; i++) {
-//     let divisors = 0;
+for (let i=n; i >= n; i++) {
+    let divisors = 0;
 
-//     if (n === 4) {}
-//     for (let j=1; j <= i; j++) {
-//         if (i % j == 0) {
-//             divisors++
-//         }
-//     }
+    if (n === 4) {}
+    for (let j=1; j <= i; j++) {
+        if (i % j == 0) {
+            divisors++
+        }
+    }
 
-//     if (divisors === 2) {
-//         console.log(i)
-//         break
-//     }
-// }
+    if (divisors === 2) {
+        console.log(i)
+        break
+    }
+}
 
+/*
+Part 3: Feeling Loopy
+As a final task, solve the following practical problem regarding string processing.
+Context: A CSV file, or “Comma-Separated Values” file is traditionally used to store tabular data. You may be familiar with CSVs through past use of programs such as Microsoft Excel or Google Sheets. While each of these programs save their data in different formats to preserve style (e.g., font color or cell backgrounds), at their core, they are storing CSV data.
+CSV data looks like this:
+ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+Not very nice to look at. The “\n” is an escaped Line Feed, which indicates that the following data should begin on a new line, as follows:
+ID,Name,Occupation,Age 42,Bruce,Knight,41 57,Bob,Fry Cook,19 63,Blaine,Quiz Master,58 98,Bill,Doctor’s Assistant,26
+As you may have guessed, these values being “comma-separated” means that each comma is also a delimiter. This is why this type of data is traditionally viewed in tables. Here is how the data looks once fully parsed:
 
+*/
 str = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
 
 

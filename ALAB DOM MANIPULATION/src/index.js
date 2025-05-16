7371692c87d58c5493c073b52f485ccc6bcf2788
atsoc1993 <atsoc1993@gmail.com>
2025-05-15 20:43:03 -0400

    Reformatting

--- ALAB DOM MANIPULATION/src/index.js
+++ ALAB DOM MANIPULATION/src/index.js
@@ -2,11 +2,13 @@ let mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = `<h1>Dom Manipulation<h1/>`;
 mainEl.classList.add("flex-ctr");
+
 //let topMenuEl = `<nav id="top-menu">`
 let topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
+
 let menuLinks = [
   { text: "about", href: "/about" },
   {
@@ -36,19 +38,23 @@ let menuLinks = [
     ],
   },
 ];
+
 menuLinks.forEach((link) => {
   let newAElement = document.createElement("a");
   newAElement.href = link.href;
   newAElement.innerText = link.text;
   topMenuEl.appendChild(newAElement);
 });
+
 const subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--subMenu-bg)";
 subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
+
 let anchorTags = document.querySelectorAll("a");
+
 topMenuEl.addEventListener("click", (e) => {
   e.preventDefault();
   Array.from(e.target.parentElement.children).forEach((child) => {
@@ -56,28 +62,35 @@ topMenuEl.addEventListener("click", (e) => {
       child.classList.remove("active");
     }
   });
+
   if (e.target.nodeName !== "A") return;
+
   console.log(e.target);
+
   if (e.target.classList.contains("active")) {
     e.target.classList.remove("active");
   } else {
     e.target.classList.add("active");
     let ancorText = e.target.innerText.toLowerCase()
     let targetLink;
-    menuLinks.forEach((link)=>{
-      if(ancorText === link.text){
-        console.log(ancorText,link)
-        if(link.hasOwnProperty('subLinks')){
+
+    menuLinks.forEach((link) => {
+
+      if (ancorText === link.text) {
+        console.log(ancorText, link);
+
+        if (link.hasOwnProperty('subLinks')) {
           targetLink = link;
-          subMenuEl.style.top = '100%'
-          subMenuEl.style.backgroundColor ='blue'
-          return
-        }else{
-          subMenuEl.style.top = '0'
-          subMenuEl.style.backgroundColor ='red'
-          return
-        }
-      }
-    })
-  }
+          subMenuEl.style.top = '100%';
+          subMenuEl.style.backgroundColor = 'blue';
+          return;
+
+        } else {
+          subMenuEl.style.top = '0';
+          subMenuEl.style.backgroundColor = 'red';
+          return;
+        };
+      };
+    });
+  };
 });

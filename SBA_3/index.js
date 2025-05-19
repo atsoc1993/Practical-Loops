126c7a4396fe846722663a858e8165c16f2c3ba4
atsoc1993 <atsoc1993@gmail.com>
2025-05-19 12:16:34 -0400

    Starting SBA, thinking of making a moving div and a revolving header

---
+++ SBA_3/index.js
@@ -0,0 +1,12 @@
+let headerDiv = document.createElement('div')
+
+let header = document.createElement('h1')
+header.id = 'header'
+header.textContent = "Test"
+header.style.left = '0px'
+header.style.right = '0px'
+header.style.top = '0px'
+header.style.bottom = '0px'
+header.style.position = 'absolute'
+
+

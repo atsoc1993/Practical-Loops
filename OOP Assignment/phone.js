0b99500fed5debc83191758e743c497f4fb1a69a
atsoc1993 <atsoc1993@gmail.com>
2025-05-23 16:04:37 -0400

    Reorganizing comment lines

--- OOP Assignment/phone.js
+++ OOP Assignment/phone.js
@@ -22,6 +22,22 @@
 // 📱 getInfo(): returns brand, model, battery level, and power status
 
 
+
+// // // 🎯 Bonus Challenges 1:
+// // // - Add  field `contacts` (array of objects `{name, number}`)  (Create a new property called contacts)
+// // // - Add method `addContact(name, number)` to add contacts
+// // // - update  `makeCall(contactOrNumber)` method to accept a contact name or number string
+// // // if a number is passed function will run normally,
+// // // If name is passed, look up number by name on contacts array log ("calling {name} and their number is {number}"), if the name doesn't exist the method should throw an error
+// // // update call history to store array objects instead of simple stringw, you should save {contactName:null if there is no contact, number,timeOfCall:when the call was made}
+
+// // // 🎯 Bonus Challenges 2:
+// // // if the brand is samsung, there is 30% percent chance that everytime a method is invoked, print instead WARNING: Your Samsung phone is on fire! It's toast now. Goodbye
+// // // if brand is nokia, the phone never loses battery, If battery level hits 0, it automatically resets to 100% because "Nokia never dies"
+// // //If you try to call any contact whose name includes "pizza", "burger", "fries", or "fast food" (case-insensitive), the phone interrupts you with this message:
+// // //"Alert! 🚨 Fast food hotline detected. Your phone just ordered a gym membership on your behalf. Time to sweat it out! 🏃‍♂️🔥"
+
+
 // = is assigning a value eg; x = 1
 // == check if these two things are the same, also don't care if they are different types eg; 2 == '2' (true)
 // === check if these two things are the same AND the same type eg; 2 === 2
@@ -219,25 +235,6 @@ class Phone {
 
 // num = 5
 // console.log(typeof num === 'number')
-
-
-// // // 🎯 Bonus Challenges 1:
-// // // - Add  field `contacts` (array of objects `{name, number}`)  (Create a new property called contacts)
-// // // - Add method `addContact(name, number)` to add contacts
-// // // - update  `makeCall(contactOrNumber)` method to accept a contact name or number string
-// // // if a number is passed function will run normally,
-// // // If name is passed, look up number by name on contacts array log ("calling {name} and their number is {number}"), if the name doesn't exist the method should throw an error
-// // // update call history to store array objects instead of simple stringw, you should save {contactName:null if there is no contact, number,timeOfCall:when the call was made}
-
-// // // 🎯 Bonus Challenges 2:
-// // // if the brand is samsung, there is 30% percent chance that everytime a method is invoked, print instead WARNING: Your Samsung phone is on fire! It's toast now. Goodbye
-// // // if brand is nokia, the phone never loses battery, If battery level hits 0, it automatically resets to 100% because "Nokia never dies"
-// // //If you try to call any contact whose name includes "pizza", "burger", "fries", or "fast food" (case-insensitive), the phone interrupts you with this message:
-// // //"Alert! 🚨 Fast food hotline detected. Your phone just ordered a gym membership on your behalf. Time to sweat it out! 🏃‍♂️🔥"
-
-
-
-
 
 
 

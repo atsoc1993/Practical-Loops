7f23b61e4e0be419238cee360bb20a746f1bc2fe
atsoc1993 <atsoc1993@gmail.com>
2025-06-04 15:44:46 -0400

    Completed additional instructions in ALAB Working With Typescript

--- ALAB Working With Typescript/index.ts
+++ ALAB Working With Typescript/index.ts
@@ -1,5 +1,5 @@
 class Vehicle {
-  status: string = "stopped";
+  status: "started" | "stopped" | "running" = "started"
   make: string;
   model: string;
   wheels: string | number;
@@ -30,7 +30,7 @@ class MotorCycle extends Vehicle {
   }
 }
 
-function printStatus(vehicle: Vehicle | Car | MotorCycle) {
+function printStatus(vehicle: Vehicle ) {
   if (vehicle.status === "running") {
     console.log("The vehicle is running.");
   } else {
@@ -51,3 +51,52 @@ if (typeof myBuick.wheels === "number") {
 }
 console.log(myBuick.wheels);
 console.log(myBuick.model);
+
+
+class NCycle<T> {
+  status: "started" | "stopped" | "running" = "started"
+  make: T | T[];
+  model: T | T[];
+  wheels: string | number;
+  
+  constructor(make: T | T[], model: T | T[], wheels: string | number) {
+    this.make = make;
+    this.model = model;
+    this.wheels = wheels;
+  }
+  start() {
+    this.status = "started";
+  }
+  stop() {
+    this.status = "stopped";
+  }
+  print(num: number | 0) : void {
+    if (typeof this.make === 'string') {
+      console.log(`This is a ${this.make} ${this.model} NCycle`)
+    } else if (Array.isArray(this.make) && Array.isArray(this.model)) { 
+      console.log(`This NCycle has a ${this.make[num]} ${this.model[num]} at Index: ${num}`)
+    } else {
+      console.log("This NCycle was not created properly")
+    }
+  }
+  printAll(): void {
+    if (Array.isArray(this.make) && Array.isArray(this.model)) {
+      for (let i in this.make) {
+        console.log(`This NCycle has a ${this.make[i]} ${this.model[i]} at Index: ${i}`)
+      }
+    } else if (typeof this.make === 'string') {
+      console.log(`This is a ${this.make} ${this.model} NCycle`)
+
+    } else {
+      console.log("This NCycle was not created properly")
+    }
+
+  }
+}
+
+// let nCycleTest = new NCycle<string[]>(["2025", "2024"], ["Nissan", "Hyundai"], "four")
+// let nCycleTest = new NCycle<string>("2025", "Nissan", "four")
+let nCycleTest = new NCycle<number>(1, 2, "four")
+console.log(nCycleTest)
+
+nCycleTest.printAll();

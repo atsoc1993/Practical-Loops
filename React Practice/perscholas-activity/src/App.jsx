8a984643722baba6aa66760d578a5d4bf868b083
atsoc1993 <atsoc1993@gmail.com>
2025-06-23 15:59:33 -0400

    Finalized

--- React Practice/perscholas-activity/src/App.jsx
+++ React Practice/perscholas-activity/src/App.jsx
@@ -2,32 +2,69 @@ import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className='main-div'>
       <h1>Employee Directory</h1>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>James King</h2>
+          <p>President and CEO</p>
+        </div>
       </div>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>Julie Taylor</h2>
+          <p>VP of Marketing</p>
+        </div>
       </div>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>Eugene Lee</h2>
+          <p>CFO</p>
+        </div>
       </div>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>John Williams</h2>
+          <p>VP of Engineering</p>
+        </div>
       </div>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>Ray Moore</h2>
+          <p>VP of Sales</p>
+        </div>
       </div>
-      <div className='employee'>
-        <h2>James King</h2>
-        <p>President and CEO</p>
+
+      <div className='employee-outer-div'>
+        <div className='image-div'>
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/007/407/996/small_2x/user-icon-person-icon-client-symbol-login-head-sign-icon-design-vector.jpg'></img>
+        </div>
+        <div className='.employee-info-div'>
+          <h2>Paul Jones</h2>
+          <p>QA Manager</p>
+        </div>
       </div>
+
     </div>
   )
 }

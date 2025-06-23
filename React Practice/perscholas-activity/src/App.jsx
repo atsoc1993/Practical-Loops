82767e3ecc7958e9ab9fee9845615c1d0a678b2f
atsoc1993 <atsoc1993@gmail.com>
2025-06-23 15:17:32 -0400

    Adding first react activity

---
+++ React Practice/perscholas-activity/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Employee Directory</h1>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+      <div className='employee'>
+        <h2>James King</h2>
+        <p>President and CEO</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
